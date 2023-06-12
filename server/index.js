28c08a659b79caa27c222e6e61e85e21083ca15d
Patrick Nilan <nilan.patrick@gmail.com>
2023-06-12 14:58:09 -0700

    added json parsing middleware

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
-let app = express();
+const app = express();
 
-app.use(express.static('client/dist'));
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
+app.use(express.static('client/dist'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.post('/repos', function (req, res) {
+  const data = req.body.username;
+  console.log(data);
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  res.send(JSON.stringify(data));
 });
 
 app.get('/repos', function (req, res) {
